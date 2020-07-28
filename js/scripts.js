956a414147ea3b6a11bad74b4b80a59cdbf2109d
evrimcoskun <evrimcoskun@gmail.com>
2020-07-28 11:21:01 +0200

    added the initial variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello World!');
-var favoriteFood = 'watermelon'
-document.write(favoriteFood)
+var pokemonList = [
+  { name: 'Bulbasaur', height: 7, types: 'grass', 'poison'},
+  { name: 'Ivysaur', height: 10, types: 'grass', 'poison'},
+  { name: 'Venusaur', height: 20, types: 'grass', 'poison'}
+];
