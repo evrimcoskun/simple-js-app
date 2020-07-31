effdecfc7176f2138c33f850188be8ec804a4190
evrimcoskun <evrimcoskun@gmail.com>
2020-07-31 17:00:15 +0200

    fixed typos in types array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 var pokemonList = [
-  { name: 'Bulbasaur', height: 7, types: 'grass', 'poison'},
-  { name: 'Ivysaur', height: 10, types: 'grass', 'poison'},
-  { name: 'Venusaur', height: 20, types: 'grass', 'poison'}
+  { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+  { name: 'Ivysaur', height: 10, types: ['grass', 'poison']},
+  { name: 'Venusaur', height: 20, types: ['grass', 'poison']}
 ];
