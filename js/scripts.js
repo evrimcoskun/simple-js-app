458d7f32af4c617be36243e719e34f6bf19e4a39
evrimcoskun <evrimcoskun@gmail.com>
2020-10-07 15:07:13 +0200

    Added a loop and a condition + minor correction
    Added a loop and a condition + minor correction

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,11 @@ var pokemonList = [
   { name: 'Ivysaur', height: 10, types: ['grass', 'poison']},
   { name: 'Venusaur', height: 20, types: ['grass', 'poison']}
 ];
+
+for (var i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 10) {
+    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ') Wow!!' + '</p>') 
+  } else { 
+    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + '</p>')
+  }
+}
