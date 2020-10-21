b99413a55cc27afdb1f987fe7d9d731eed5e68d7
evrimcoskun <evrimcoskun@gmail.com>
2020-10-21 23:14:43 +0200

    added foreach instead of for loop

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,18 @@ var pokemonList = [
   { name: 'Venusaur', height: 20, types: ['grass', 'poison']}
 ];
 
-for (var i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 10) {
-    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ') Wow!!' + '</p>') 
-  } else { 
-    document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + '</p>')
+// for (var i = 0; i < pokemonList.length; i++) {
+//   if (pokemonList[i].height > 10) {
+//     document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ') Wow!!' + '</p>') 
+//   } else { 
+//     document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + '</p>')
+//   }
+// }
+
+pokemonList.forEach(function(pokemon){
+  if (pokemon.height > 10) {
+    document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ') Wow!!' + '</p>') 
+  }else{ 
+    document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ')' + '</p>')
   }
-}
+});
