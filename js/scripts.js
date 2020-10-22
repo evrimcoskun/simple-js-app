788e2115f8eabbf0661afdecba58456ef52a6abd
evrimcoskun <evrimcoskun@gmail.com>
2020-10-22 21:44:18 +0200

    created an IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,31 @@
-var pokemonList = [
-  { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
-  { name: 'Ivysaur', height: 10, types: ['grass', 'poison']},
-  { name: 'Venusaur', height: 20, types: ['grass', 'poison']}
-];
+var pokemonRepository = (function () {
+  var pokemonList = [  
+    { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
+    { name: 'Ivysaur', height: 10, types: ['grass', 'poison']},
+    { name: 'Venusaur', height: 20, types: ['grass', 'poison']}
+  ];
+
+  function getAll() {
+    return pokemonList;
+  }
+  
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  };
+})();
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  if (pokemon.height > 10) {
+    document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ') Wow!!' + '</p>') 
+  }else{ 
+    document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ')' + '</p>')
+  }
+});
 
 // for (var i = 0; i < pokemonList.length; i++) {
 //   if (pokemonList[i].height > 10) {
@@ -11,11 +34,3 @@ var pokemonList = [
 //     document.write('<p>' + pokemonList[i].name + ' (height:' + pokemonList[i].height + ')' + '</p>')
 //   }
 // }
-
-pokemonList.forEach(function(pokemon){
-  if (pokemon.height > 10) {
-    document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ') Wow!!' + '</p>') 
-  }else{ 
-    document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ')' + '</p>')
-  }
-});
