9c34ae083de4cdb6e4d9620677621745df00e601
evrimcoskun <evrimcoskun@gmail.com>
2020-10-27 21:08:51 +0100

    added data to the dom, event handler & basic styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var pokemonRepository = (function () {
+var pokemonRepository = (function() {
   var pokemonList = [  
     { name: 'Bulbasaur', height: 7, types: ['grass', 'poison']},
     { name: 'Ivysaur', height: 10, types: ['grass', 'poison']},
@@ -12,19 +12,35 @@ var pokemonRepository = (function () {
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
+  
+  function showDetails(pokemon) {
+    console.log(pokemon)
+  }
+  
+  function addListItem(pokemon) {
+    var pokemonList = document.querySelector('.pokemon-list');
+    var listItem = document.createElement('li');
+    var button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add('button-class');
+    listItem.appendChild(button);
+    pokemonList.appendChild(listItem);
+    button.addEventListener('click', function() {
+      showDetails(pokemon)
+    })
+  }
 
   return {
     getAll: getAll,
-    add: add
+    add: add,
+    addListItem: addListItem
+    
   };
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon){
-  if (pokemon.height > 10) {
-    document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ') Wow!!' + '</p>') 
-  }else{ 
-    document.write('<p>' + pokemon.name + ' (height:' + pokemon.height + ')' + '</p>')
-  }
+pokemonRepository.getAll().forEach(function(pokemon) {
+  pokemonRepository.addListItem(pokemon);
+
 });
 
 // for (var i = 0; i < pokemonList.length; i++) {
